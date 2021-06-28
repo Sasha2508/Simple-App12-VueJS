0ad561a3ee6bc5195a9809e82ce489ba202d38f2
Sasha2508 <71674667+Sasha2508@users.noreply.github.com>
2021-06-28 17:02:49 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+<section>
+    <header><h1>Users</h1>
+    </header>
+    <userr-data 
+    @add-user="addUser"> 
+    </userr-data>
+    <active-user
+    v-for="user in users"
+    :key="user.id"
+    :name="user.name"
+    :age="user.age"
+    > 
+    </active-user>
+    </section>
+
+</template>
+
+<script>
+import ActiveUser from './components/ActiveUser.vue';
+import UserrData from './components/UserrData.vue';
+
+export default {
+    components: {UserrData, ActiveUser},
+    data() {
+        return {
+            users: [
+                {
+                    id: 'user1',
+                    name: 'Alex',
+                    age: '23'
+                },
+                {
+                    id: 'user2',
+                    name: 'Sam',
+                    age: '20'
+                },
+            ],
+
+        };
+    },
+    methods: {
+        addUser(name, age) {
+            const newUser = {
+                id: new Date().toISOString(),
+                name: name,
+                age: +age
+            };
+            this.users.push(newUser);
+        },
+    },
+};
+
+</script>
+<style>
+html {
+    font-family: sans-serif;
+}
+div {
+    margin: 2rem auto;
+    max-width: 40rem;
+    border-radius: 12px;
+    border: 1px solid #ccc;
+    padding: 1rem ;
+}
+</style>
---
+++ src/components/UserrData.vue
@@ -0,0 +1,30 @@
+<template>
+        <section>
+            <form @submit.prevent="submitData">
+            <input type="text" placeholder="Your Name" v-model="enteredName" />
+            <input type="age" placeholder="Your Age" v-model="enteredAge" />
+            <button>Add User</button>
+            </form>
+            </section>
+
+            
+</template>
+
+<script>
+
+
+export default {
+    emits: ['add-user'],
+    data() {
+        return {
+            enteredName: '',
+            enteredAge: ''
+        };
+    },
+    methods: {
+        submitData() {
+            this.$emit('add-user', this.enteredName, this.enteredAge);
+        }
+    }
+};
+</script>
